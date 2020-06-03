c698dd5503189b768beb74771bc05821275669f0
Elias Martinez <jemart218@gmail.com>
2020-06-02 18:46:20 -0700

    shopping done

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+$(function handleSubmission() {
+
+  $('#js-shopping-list-form').submit( event => {
+    event.preventDefault();
+    const newItem = $(event.currentTarget).find(
+      'input[name="shopping-list-entry"]').val();
+  
+
+    $('.shopping-list').append(`<li>
+    <span class="shopping-item">${newItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+    </li>`);
+  
+  });
+
+  $('.shopping-list').on('click','button:first-child',function(event){
+    $(this).parent().siblings('span').toggleClass('shopping-item__checked')
+  });
+
+  $('.shopping-list').on('click','button:nth-child(2)',function(event){
+    $(this).parent().parent().remove()
+  })
+
+});
